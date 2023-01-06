4233e951fc165a92a7585d11a09cb1a24ee4fc73
Curtis <cmbitton@gmail.com>
2023-01-06 17:43:00 -0500

    Wrtie function to update gameboard

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+//gameboard
 const gameBoard = (function () {
-    const boardValues = [];
+    const boardValues = [['X', null, 'O'], [null, null, null], [null, null, null]];
     const loadBoard = () => {
-        const board = document.querySelector('.game-board');
+        const board = document.querySelectorAll('.board-tile');
+        for (const tile of board){
+            const boardRow = +tile.getAttribute('data-row');
+            const boardColumn = +tile.getAttribute('data-column');
+            tile.textContent = boardValues[boardRow - 1][boardColumn - 1]
+        }
     }
+    return {loadBoard}
 })();
+
+gameBoard.loadBoard();
