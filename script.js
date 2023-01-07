d7c3bbc2090dafad462999e68c125657e873597c
Curtis <cmbitton@gmail.com>
2023-01-07 18:49:50 -0500

    Added infomod which displays game info
    Game now displays player turns and game result. Also added some CSS
    styling.

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ const gameBoard = (function () {
             const boardRow = +tile.getAttribute('data-row');
             const boardColumn = +tile.getAttribute('data-column');
             tile.textContent = boardValues[boardRow - 1][boardColumn - 1];
+            if(boardValues[boardRow - 1][boardColumn - 1] !== null) {
+                tile.classList.add('selected')
+            };
         }
     }
 
@@ -58,40 +61,67 @@ const player = (name, number) => {
     return { name, number };
 }
 
+const infoMod = (function() {
+    const infoScreen = document.querySelector('.game-info');
+    
+    const removeHiddenClass = () => {
+        infoScreen.classList.remove('hidden');
+    }
+    
+    const alertPlayerTurn = (number) => {
+        infoScreen.textContent = `Player ${number} - Your Turn!`;
+    }
+
+    const alertPlayerWin = (number) => {
+        if (number <= 2){
+        infoScreen.textContent = `Player ${number} is the Winner!`;}
+        else {
+            infoScreen.textContent = 'Tie!';
+        }
+    }
+    return { alertPlayerTurn, removeHiddenClass, alertPlayerWin }
+})();
+
 const game = (function () {
     let gameWon = false;
-    
     const playGame = () => {
         const playButton = document.querySelector('.play-button');
-        const gameBoard = document.querySelector('.game-board');
+        const board = document.querySelector('.game-board');
         playButton.addEventListener('click', () => {
             playButton.classList.add('hidden');
-            gameBoard.classList.remove('hidden');
+            board.classList.remove('hidden');
+            infoMod.removeHiddenClass();
         })
     }
 
     const displayWin = () => {
-        if(gameBoard.checkForWin('X') || gameBoard.checkForWin('O')) {
-            console.log('WIN!');
+        if(gameBoard.checkForWin('X')) {
+            infoMod.alertPlayerWin(1);
+            gameWon = true;
+        }
+        else if(gameBoard.checkForWin('O')) {
+            infoMod.alertPlayerWin(2);
             gameWon = true;
         }
         else if (gameBoard.checkForWin('X') === false){
-            console.log('tie')
+            infoMod.alertPlayerWin(3);
         }
     }
 
     const placeMarker = () => {
-        const board = document.querySelectorAll('.board-tile');
-        for (const tile of board) {
+        const boardTiles = document.querySelectorAll('.board-tile');
+        for (const tile of boardTiles) {
             tile.addEventListener('click', (e) => {
                 const markerRow = +e.target.getAttribute('data-row');
                 const markerColumn = +e.target.getAttribute('data-column');
                 const count = gameBoard.getCount();
                 if(count.amountX > count.amountO && gameWon === false){
                     gameBoard.updateBoardValues(markerRow, markerColumn, 2);
+                    infoMod.alertPlayerTurn(1);
                 }
                 else if (gameWon === false){
                     gameBoard.updateBoardValues(markerRow, markerColumn, 1);
+                    infoMod.alertPlayerTurn(2);
                 }
                 
                 gameBoard.loadBoard();
