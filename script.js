9324024875b270796f1fa272c616e373d29b049f
Curtis <cmbitton@gmail.com>
2023-01-07 14:31:07 -0500

    Add checkforwin function
    Added function that checks for win and prevents user selection after a
    win.

--- script.js
+++ script.js
@@ -23,13 +23,32 @@ const gameBoard = (function () {
 
     const updateBoardValues = (row, column, player) => {
         if ((boardValues[row - 1][column - 1] === null)){
-        (player === 1) ? boardValues[row - 1][column - 1] = 'X' : boardValues[row - 1][column - 1] = 'O'
+        (player === 1) ? boardValues[row - 1][column - 1] = 'X' : boardValues[row - 1][column - 1] = 'O';
         }
+    }
+
+    const checkForWin = (letter) => {
+        if (//win by row
+            (boardValues[0][0] === letter && boardValues[0][1] === letter && boardValues[0][2] === letter) ||
+            (boardValues[1][0] === letter && boardValues[1][1] === letter && boardValues[1][2] === letter) ||
+            (boardValues[2][0] === letter && boardValues[2][1] === letter && boardValues[2][2] === letter) ||
+            //win by column
+            (boardValues[0][0] === letter && boardValues[1][0] === letter && boardValues[2][0] === letter) ||
+            (boardValues[0][1] === letter && boardValues[1][1] === letter && boardValues[2][1] === letter) ||
+            (boardValues[0][2] === letter && boardValues[1][2] === letter && boardValues[2][2] === letter) ||
+            //win diagonally
+            (boardValues[0][0] === letter && boardValues[1][1] === letter && boardValues[2][2] === letter) ||
+            (boardValues[2][0] === letter && boardValues[1][1] === letter && boardValues[0][2] === letter)
+        ) {
+            return true;
+        }
+
     }
 
     return { loadBoard,
              updateBoardValues,
-             getCount}
+             getCount,
+            checkForWin }
 })();
 
 const player = (name, number) => {
@@ -37,6 +56,7 @@ const player = (name, number) => {
 }
 
 const game = (function () {
+    let gameWon = false;
     const placeMarker = () => {
         const board = document.querySelectorAll('.board-tile');
         for (const tile of board) {
@@ -44,14 +64,17 @@ const game = (function () {
                 const markerRow = +e.target.getAttribute('data-row');
                 const markerColumn = +e.target.getAttribute('data-column');
                 const count = gameBoard.getCount();
-                if(count.amountX > count.amountO){
+                if(count.amountX > count.amountO && gameWon === false){
                     gameBoard.updateBoardValues(markerRow, markerColumn, 2);
                 }
-                else{
+                else if (gameWon === false){
                     gameBoard.updateBoardValues(markerRow, markerColumn, 1);
                 }
                 
                 gameBoard.loadBoard();
+                if(gameBoard.checkForWin('X') || gameBoard.checkForWin('O')) {
+                    console.log('WIN!');
+                    gameWon = true;}
 
             })
         }
